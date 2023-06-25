753eb1aae1afb37d322e63d710b44dffc20fd933
GastonBernardini <124274459+GastonBernardini@users.noreply.github.com>
2023-06-25 04:09:17 -0300

    UI changes in BossHealthBar
    Se agrego la barra de vida a DIO ademas de que su valor de reduzca en base a el daño que recibe

--- Desarrollo de juegos en tiempo real-Trabajo1/Assets/Scripts/Player/HealthBar.cs
+++ Desarrollo de juegos en tiempo real-Trabajo1/Assets/Scripts/Player/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,42 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider sliderHealth;
+    public GameObject target;
 
-    public void SetMaxHealth (int health)
+    private bool isTargetActive;
+
+    private void Start()
+    {
+        isTargetActive = false;
+        sliderHealth.gameObject.SetActive(true);
+    }
+
+    public void SetMaxHealth(int health)
     {
         sliderHealth.maxValue = health;
         sliderHealth.value = health;
     }
 
-    public void SetHealth (int health)
+    public void SetHealth(int health)
     {
-        sliderHealth.value = health; 
+        sliderHealth.value = health;
     }
 
+    public void Update()
+    {
+        if (sliderHealth.value == 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // Verificar si el objetivo está activo en la escena
+        if (target != null && target.activeSelf != isTargetActive)
+        {
+            isTargetActive = target.activeSelf;
+            sliderHealth.gameObject.SetActive(isTargetActive);
+        }
+    }
 }
+
+
+
